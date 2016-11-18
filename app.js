33ed69520d1072f1d294a8a0971aff886bc46631
Sophia Pozzi <sopozzi@gmail.com>
2016-11-17 20:32:11 -0800

    infinite scroll work

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 //API key: h6i15byym5wi26bk17mg9yy2
 //API Secret: 26jcl2rntx
+var OFFSET = 50;
+var SEARCHTERM = '';
 
 $(function(){
-	$('.results').hide();
-	$('#etsySearch').submit(function(e){
-		 e.preventDefault();
-		if( $('#etsyQuery').val().length === 0) {return false};
-		var searchTerm = $('#etsyQuery').val();
-		etsyData(searchTerm);
-
-	});
+	
 });
-var etsyData = function(searchTerm) {
+var etsyData = function(searchTerm, offset) {
 	var listingRequest = {
 		api_key: 'h6i15byym5wi26bk17mg9yy2',
 		method : 'GET', 
@@ -19,6 +14,8 @@ var etsyData = function(searchTerm) {
 		fields : ['listing_id', 'price', 'title', 'listing_image_id', 'url_170x135'], 
 		includes: 'MainImage',
 		limit: 50,
+		offset: offset ? offset : 0
+
 	};
 	$.ajax({
 		url : 'https://openapi.etsy.com/v2/listings/active.js',
@@ -32,7 +29,6 @@ var etsyData = function(searchTerm) {
 
 function showResults(results){
 	var html = "";
-	var searchTerm = $('#etsyQuery').val();
 	$.each(results.results, function(index, value){
 		html += '<li><a href=" ' + value.url + ' " target="_blank" ">' + '</br>';
 		html += '<img src=" '+ value.MainImage.url_570xN+ ' "/>' + '</a>' + '</br>';
@@ -40,43 +36,37 @@ function showResults(results){
 		html += 'USD' + " " + value.price;
 		html += '<br>' + '<button class="button"><a href=" ' + value.url + ' " target="_blank" ">view on etsy</a></button>' + '</li>';	
 	});
-	$('#searchResults').html(html);
+	$('#searchResults').append(html);
 	$('#etsyQuery').val('');
-	$('.results').show();
-	$('.search').text(searchTerm);
+	$('.resultsText').show();
+	$('.search').text(SEARCHTERM);
+	$('#loading').hide();
 
 }
 
 $(document).ready(function() {
-	var win = $(window);
-	var searchTerm = $('#etsyQuery').val();
-	var html = "";
-	var offset = 0;
-	// Each time the user scrolls
-	win.scroll(function() {
-		// End of the document reached?
-		if ($(document).height() - win.height() == win.scrollTop()) {
-			$('#loading').show();
-			var addListingRequest = {
-				api_key: 'h6i15byym5wi26bk17mg9yy2',
-				method : 'GET', 
-				keywords : searchTerm,
-				fields : ['listing_id', 'price', 'title', 'listing_image_id', 'url_170x135'], 
-				includes: 'MainImage',
-				limit: 50,
-				offset: 50
-			};
-			$.ajax({
-				url : 'https://openapi.etsy.com/v2/listings/active.js',
-				data : addListingRequest,
-				success : function(data, e){
-					$('#searchResults').append(html);
-				},
-				dataType : 'jsonp'
-			});
-		}
-	});
-	offset +=1;
-	
+	$('.resultsText').hide();
 	$('#loading').hide();
+	$('#etsySearch').submit(function(e){
+		 e.preventDefault();
+		if( $('#etsyQuery').val().length === 0) {return false};
+		$('searchResults').html('');
+		SEARCHTERM = $('#etsyQuery').val();
+		OFFSET = 50;
+		etsyData(SEARCHTERM);
+
+	});
+	// Each time the user scrolls
+	$(window).scroll(function() {
+		console.log(Math.floor($(window).scrollTop()));
+		// End of the document reached?
+		if(Math.floor($(window).scrollTop()) + $(window).height() == $(document).height() - 5) {
+       	alert("bottom!");
+       	$('#loading').show();
+			etsyData(SEARCHTERM, OFFSET);
+			OFFSET += 50;
+   		} 
+			
+		
+	});
 });
